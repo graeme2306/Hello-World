342630e00e05af4ea842096dbe4fa3cdfed823a6
graeme2306 <webstergraeme@yahoo.com.au>
2016-04-16 06:52:55 +0930

    Added files via upload

---
+++ jax.swift
@@ -0,0 +1,8 @@
+print("The swift language is available on Linux")
+print("\nAnd this is a newline.")
+func stutter() ->String{
+    return "This is a sttttt u ttrerrrrr like no other.\n"
+}
+var guff: String = stutter()
+print(guff)
+
